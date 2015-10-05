42331207d408bd23b08ddbdaf72e707723b1344d
adityamenon-iom <aditya.menon@io-media.com>
2015-10-06 02:22:26 +0530

    Create google2.3.go

---
+++ google2.3.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Result string
+type Search func(query string) Result
+
+func First(query string, replicas ...Search) Result {
+	c := make(chan Result)
+	searchReplica := func(i int) { c <- replicas[i](query) }
+	for i := range replicas {
+		go searchReplica(i)
+	}
+	return <-c
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	result := First("golang",
+		fakeSearch("replica 1"),
+		fakeSearch("replica 2"))
+	elapsed := time.Since(start)
+	fmt.Println(result)
+	fmt.Println(elapsed)
+}
+
+func fakeSearch(kind string) Search {
+        return func(query string) Result {
+	          time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	          return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+        }
+}
