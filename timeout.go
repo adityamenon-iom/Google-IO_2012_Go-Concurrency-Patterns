8953c318a5f90630f410630ab4b622429c1cc006
adityamenon-iom <aditya.menon@io-media.com>
2015-10-06 02:32:26 +0530

    Create timeout.go

---
+++ timeout.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	c := boring("Joe")
+	for {
+		select {
+		case s := <-c:
+			fmt.Println(s)
+		case <-time.After(1 * time.Second):
+			fmt.Println("You're too slow.")
+			return
+		}
+	}
+}
+
+func boring(msg string) <-chan string { // Returns receive-only channel of strings.
+	c := make(chan string)
+	go func() { // We launch the goroutine from inside the function.
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%s: %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+		}
+	}()
+	return c // Return the channel to the caller.
+}
+
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			select {
+			case s := <-input1:
+				c <- s
+			case s := <-input2:
+				c <- s
+			}
+		}
+	}()
+	return c
+}
