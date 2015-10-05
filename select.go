d390d18063a6b1263ecaea2df51b5da50b9d1705
adityamenon-iom <aditya.menon@io-media.com>
2015-10-06 02:29:19 +0530

    Create select.go

---
+++ select.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	var c1, c2, c3 chan int
+
+	select {
+	case v1 := <-c1:
+		fmt.Printf("received %v from c1\n", v1)
+	case v2 := <-c2:
+		fmt.Printf("received %v from c2\n", v1)
+	case c3 <- 23:
+		fmt.Printf("sent %v to c3\n", 23)
+	default:
+		fmt.Printf("no one was ready to communicate\n")
+	}
+}
